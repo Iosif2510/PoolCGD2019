0f1aad253588a2e86b7063bc596a0f16057f4a2d
Iosif2510 <gpotatok@gmail.com>
2019-08-23 03:20:23 +0900

    Highscore on gameover bug fix

--- Practice/Assets/Script/GameUI.cs
+++ Practice/Assets/Script/GameUI.cs
@@ -171,6 +171,7 @@ public class GameUI : MonoBehaviour
         if (MapGenerator.Instance.currentMode == MapGenerator.GameMode.Infinite) {
             StartCoroutine(Fade(Color.clear, new Color(0, 0, 0, .9f), 1));
             gameOverScoreUI.text = scoreUI.text;
+            if (ScoreKeeper.score > ScoreKeeper.highscore) highScoreUI.text = "New Highscore!";
             highScoreUI.text = $"HighScore: {ScoreKeeper.highscore.ToString("D6")}";
             inGameUI.SetActive(false);
             Cursor.visible = true;
